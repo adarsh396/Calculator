49d8b017ff0db49aae99c7b0625fbe4082307ad0
adarsh396 <adadarsh396@gmail.com>
2024-08-28 11:36:44 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const display = document.getElementById("display");
+
+function appendToDisplay(input) {
+  display.value += input;
+}
+function ClearDisplay() {
+  display.value = "";
+}
+function calculate() {
+  display.value = eval(display.value);
+}
